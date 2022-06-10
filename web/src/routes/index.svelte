4128b3960a4fea75a87fae68fa0e25b1153b772e
Frederik <frederik.vincent@hotmail.com>
2022-06-10 02:18:48 +0200

    Remove tis variable, add no search result handling, add accordion for filters

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -10,7 +10,7 @@
 
 	let search: string
 	let skip = 0
-	let limit = 10
+	let limit = 20
 	let insightQuery = `
       query MyQuery {
         getInsight(skip: ${skip}, limit: ${limit}) {
@@ -59,7 +59,6 @@
 	// 		console.log('lol')
 	// 		return result.data.getInsight
 	// 	})
-	let tis
 
 	// The "oldschool" way
 	const fetchInsight = async (): Promise<Insight[]> => {
@@ -74,14 +73,13 @@
 		})
 
 		const result = await res.json()
-		tis = result.data.getInsight
 		return result.data.getInsight
 	}
 
 	let data: Promise<Insight[]> | Insight[] = fetchInsight()
 
-	const refresh = () => {
-		data = fetchInsight()
+	const refresh = async () => {
+		data = await fetchInsight()
 	}
 
 	// Holds table sort state.  Initialized to reflect table sorted by id column ascending.
@@ -111,94 +109,113 @@
 		}
 	}
 
-	$: visibleData = search
-		? tis.filter((user) => {
-				// TODO: This is really ugly and only surves to make sure we dont
-				// run .toLowercase on undefined values
-				const employeeName =
-					user.employeeName && user.employeeName.toLowerCase().match(`${search.toLowerCase()}.*`)
-				const employeeEmail =
-					user.employeeEmail && user.employeeEmail.toLowerCase().match(`${search.toLowerCase()}.*`)
-				const departmentName =
-					user.departmentName &&
-					user.departmentName.toLowerCase().match(`${search.toLowerCase()}.*`)
-				const managersName =
-					user.managersName && user.managersName.toLowerCase().match(`${search.toLowerCase()}.*`)
-				const id = user.id && user.id.toLowerCase().match(`${search.toLowerCase()}.*`)
+	let visibleData
+	$: {
+		visibleData = search
+			? data.filter((user) => {
+					// TODO: This is really ugly and only surves to make sure we dont
+					// run .toLowercase on undefined values
+					const employeeName =
+						user.employeeName && user.employeeName.toLowerCase().match(`${search.toLowerCase()}.*`)
+					const employeeEmail =
+						user.employeeEmail &&
+						user.employeeEmail.toLowerCase().match(`${search.toLowerCase()}.*`)
+					const departmentName =
+						user.departmentName &&
+						user.departmentName.toLowerCase().match(`${search.toLowerCase()}.*`)
+					const managersName =
+						user.managersName && user.managersName.toLowerCase().match(`${search.toLowerCase()}.*`)
+					const id = user.id && user.id.toLowerCase().match(`${search.toLowerCase()}.*`)
 
-				return employeeName || employeeEmail || departmentName || managersName || id
-		  })
-		: tis
+					return employeeName || employeeEmail || departmentName || managersName || id
+			  })
+			: data
+	}
 </script>
 
 <h1>Velkommen til indsigt</h1>
 
-<label for="skip">Skip</label>
-<input name="skip" type="number" min="0" bind:value={skip} />
-<label for="limit">Limit</label>
-<input name="limit" type="number" min="0" max="1000" bind:value={limit} />
-<label for="fields">Fields</label>
+<details>
+	<summary role="button">Filtre</summary>
 
-{#each defaultFields as field}
-	<input
-		name={field}
-		type="checkbox"
-		on:change={() => {
-			if (selectedFields.includes(field)) {
-				selectedFields = selectedFields.filter((e) => e !== field)
-			} else {
-				selectedFields.push(field)
-			}
-			insightQuery = buildQuery(skip, limit, selectedFields)
-			refresh()
-		}}
-		checked
-	/>
-	{field}
-	<br />
-{/each}
+	<label for="skip">Skip</label>
+	<input name="skip" type="number" min="0" bind:value={skip} />
+	<label for="limit">Limit</label>
+	<input name="limit" type="number" min="0" max="1000" bind:value={limit} />
+	<label for="fields">Fields</label>
 
-<br />
+	<details role="list">
+		<summary aria-haspopup="listbox">Choose fields</summary>
+		<ul role="listbox">
+			{#each defaultFields as field}
+				<li>
+					<label>
+						<input
+							type="checkbox"
+							on:change={() => {
+								if (selectedFields.includes(field)) {
+									selectedFields = selectedFields.filter((e) => e !== field)
+								} else {
+									selectedFields.push(field)
+								}
+								insightQuery = buildQuery(skip, limit, selectedFields)
+								refresh()
+							}}
+							checked
+						/>{field}
+					</label>
+				</li>
+			{/each}
+		</ul>
+	</details>
+</details>
+
 <input type="search" bind:value={search} placeholder="Search" />
 
 <br />
-<table>
-	<thead>
-		<tr>
-			{#each selectedFields as header}
-				<th scope="col" on:click={() => sort(header)}>{header}</th>
-			{/each}
-		</tr>
-	</thead>
-	{#await data}
-		<tbody>
+<figure>
+	<table>
+		<thead>
 			<tr>
-				{#each selectedFields as _}
-					<th aria-busy="true" />
+				{#each selectedFields as header}
+					<th scope="col" on:click={() => sort(header)}>{header}</th>
 				{/each}
 			</tr>
-		</tbody>
-	{:then insights}
-		{#if search}
-			{#each visibleData as insight}
-				<tbody>
-					<tr>
-						{#each Object.values(insight) as field}
-							<td>{field}</td>
-						{/each}
-					</tr>
-				</tbody>
-			{/each}
-		{:else}
-			{#each insights as insight}
-				<tbody>
-					<tr>
-						{#each Object.values(insight) as field}
-							<td>{field}</td>
-						{/each}
-					</tr>
-				</tbody>
-			{/each}
-		{/if}
-	{/await}
-</table>
+		</thead>
+		{#await data}
+			<tbody>
+				<tr>
+					{#each selectedFields as _}
+						<th aria-busy="true" />
+					{/each}
+				</tr>
+			</tbody>
+		{:then insights}
+			{#if search}
+				{#each visibleData as insight}
+					<tbody>
+						<tr>
+							{#each Object.values(insight) as field}
+								<td>{field}</td>
+							{/each}
+						</tr>
+					</tbody>
+				{/each}
+			{:else}
+				{#each insights as insight}
+					<tbody>
+						<tr>
+							{#each Object.values(insight) as field}
+								<td>{field}</td>
+							{/each}
+						</tr>
+					</tbody>
+				{/each}
+			{/if}
+		{/await}
+	</table>
+</figure>
+{#if search && !visibleData.length}
+	<h4 style="text-align: center;">Ingen resultater for: <i>{search}</i></h4>
+	<p style="text-align: center;">Med felterne: <i>{selectedFields.join(', ')}</i></p>
+{/if}
